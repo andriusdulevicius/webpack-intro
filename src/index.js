463b262df4c6f3fd5a05c78bba0854953bcdfdf9
AndriusDulevicius <andriusdulevicius@email.com>
2021-05-20 09:40:29 +0300

    more functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import add from './js/add.js';
+import say from './js/say.js';
 console.log('webpack intro index.js');
+
+console.log('add(1,2,3): ', add(1, 2, 3));
+console.log('say ', say('nice day'));

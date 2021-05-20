41b39ec620d897251a8761d5f1bd5e92ad39faac
AndriusDulevicius <andriusdulevicius@email.com>
2021-05-20 11:32:59 +0300

    entry and output config

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        //nurodom musu programos pagrindini js faila , is cia viskas bus paimta
+        main: path.resolve(__dirname, './src/app.js'),
+    },
+    output: {
+        //i cia viskas bus sudeta
+        filename: 'main.bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [],
+};

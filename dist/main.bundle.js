43fdc3364b7818189bb602da2706292da439e696
AndriusDulevicius <andriusdulevicius@email.com>
2021-05-21 08:27:13 +0300

    css loaders added

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -1,1 +1,106 @@
-(()=>{"use strict";console.log("webpack intro index.js"),console.log("add(1,2,3): ",6),console.log("say ","I am saying "+"nice day");console.log("copy : ",{age:30,name:"Antanas",gender:"male"})})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _js_add_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./js/add.js */ \"./src/js/add.js\");\n/* harmony import */ var _js_say_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./js/say.js */ \"./src/js/say.js\");\n\r\n\r\n// const path = require('path');\r\n// console.log('path', path.resolve(__dirname));\r\n\r\nconsole.log('webpack intro index.js');\r\n\r\nconsole.log('add(1,2,3): ', (0,_js_add_js__WEBPACK_IMPORTED_MODULE_0__.default)(1, 2, 3));\r\nconsole.log('say ', (0,_js_say_js__WEBPACK_IMPORTED_MODULE_1__.default)('nice day'));\r\n\r\nconst obj = {\r\n    age: 30,\r\n    name: 'Antanas',\r\n};\r\n\r\nconst copy = { ...obj, gender: 'male' };\r\n\r\nconsole.log('copy : ', copy);\r\n\n\n//# sourceURL=webpack://webpack-intro/./src/app.js?");
+
+/***/ }),
+
+/***/ "./src/js/add.js":
+/*!***********************!*\
+  !*** ./src/js/add.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ add)\n/* harmony export */ });\nfunction add(a, b, c) {\r\n    return a + b + c;\r\n}\r\n\n\n//# sourceURL=webpack://webpack-intro/./src/js/add.js?");
+
+/***/ }),
+
+/***/ "./src/js/say.js":
+/*!***********************!*\
+  !*** ./src/js/say.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst say = (whatToSay) => {\r\n    return 'I am saying ' + whatToSay;\r\n};\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (say);\r\n\n\n//# sourceURL=webpack://webpack-intro/./src/js/say.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/app.js");
+/******/ 	
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
         filename: 'main.bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Webpack intro dynamic',
